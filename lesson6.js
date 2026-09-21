b1197135a51660ca79d9d36e549430662a8f99d0
masarogo <alejandrasilvama@gmail.com>
2026-09-21 14:44:21 -0300

    conditional statements

---
+++ lesson6.js
@@ -0,0 +1,31 @@
+//Conditional Statement -- needed when you need to split the flow of your app based on the defined condition
+
+// if(condition){
+    //execute some code here
+// } else{
+    //execute some code here
+// }
+
+//If hour between 6 and 12 print "Good morning"
+//If hour between 12 and 18 print "Good afternoon"
+//Otherwise: "Good evening"
+
+var hour = 6
+
+if(hour >=6 && hour < 12){
+    console.log('Good morning')
+} else if (hour >=12 && hour <18){
+    console.log('Good afternoon')
+} else {
+    console.log('Good evening')
+}
+
+//Variable de lesson5
+var ageIsMoreThanEighteen = true
+var isUSCitizen = true
+
+if(ageIsMoreThanEighteen && isUSCitizen) {
+    console.log('Customer is elegible for DL')
+} else {
+    console.log('Customer is NOT elegible for DL')
+}
