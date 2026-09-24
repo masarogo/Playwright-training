4a03ed0aa57a39a57de790c0852e8f19e8f34d18
masarogo <alejandrasilvama@gmail.com>
2026-09-24 11:26:21 -0300

    Class and Methods:

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,3 +1,23 @@
 export function printAge(age) {
     console.log(age)
 }
+
+//Class and Methods: they are others forms of abstraction, similar to the function, to create reusable components of the code
+// we need to export the class so it can be visible to other files inside our project
+//customer details seria method name
+
+export class CustomerDetails {
+
+    printFirstName(firstName) {
+        console.log(firstName)
+    }
+
+    /**
+     * This method will print the last name
+     * @param {string} lastName 
+     */
+      printLastName(lastName) {
+        console.log(lastName)
+    }
+
+}
---
+++ lessons/lesson9.js
@@ -0,0 +1,6 @@
+
+import { CustomerDetails } from "../helpers/printHelper.js";
+
+var customerDetails = new CustomerDetails()
+customerDetails.printFirstName('Steve')
+customerDetails.printLastName('Smith')
