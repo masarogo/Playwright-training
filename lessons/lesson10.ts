cd2ac48815efc0b2f9b6d6b606d5bf6169ba144f
masarogo <alejandrasilvama@gmail.com>
2026-09-24 11:37:25 -0300

    Typescript vs Javascript

---
+++ lessons/lesson10.ts
@@ -0,0 +1,16 @@
+
+var customerFirstName: string = "John"
+var customerLastName = "Smith"
+var customerAge = 25
+
+customerFirstName = 100
+
+type Customer = {firstName: string, lastName: string, active: boolean }
+
+var firstCustomer: Customer = {
+    firstName: "Mary",
+    lastName: "Shelley",
+    active: true
+}
+
+//Typescript: script data typing reduces the number of mistakes you can make during the scripting, while in Javascript you cannot do that, you have to wait for the code to crash
